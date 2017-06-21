ed3adbf6636c5144c1e56a9bc840b8952b7854a7
djpillen@umich.edu <djpillen@umich.edu>
2017-06-21 11:46:45 -0400

    hide unused sections

--- frontend/assets/accession_readonly_fields.js
+++ frontend/assets/accession_readonly_fields.js
@@ -1,11 +1,19 @@
 $(function () {
-  var fields = ["accession_title_", "accession_inventory_", 
-                "accession_retention_rule_", "accession_user_defined__string_1_", 
-                "accession_restrictions_apply_", "accession_publish_",
-                "accession_access_restrictions_", "accession_resource_type_",
-                "accession_use_restrictions_"];
+  var hidden_sections = ["accession_dates_", "accession_subjects_",
+  						"accession_rights_statements_", "accession_classifications_"]
+  
+  hidden_sections.map(function (section) {
+  	$('section[id$=' + section + ']').hide();
+  	$('li[class^=sidebar-entry][class$=' + section + ']').hide();
+  });
+  
+  var readonly_fields = ["accession_title_", "accession_inventory_", 
+            			"accession_retention_rule_", "accession_user_defined__string_1_", 
+            			"accession_restrictions_apply_", "accession_publish_",
+            			"accession_access_restrictions_", "accession_resource_type_",
+            			"accession_use_restrictions_"];
 
-  fields.map(function (field) {
+  readonly_fields.map(function (field) {
     $("#".concat(field)).attr("readonly", "readonly");
     $("#".concat(field)).prop("disabled", true);
   });
