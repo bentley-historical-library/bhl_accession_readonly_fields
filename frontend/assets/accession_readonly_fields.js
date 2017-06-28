dd89c022b4aa481fdf0f74548c63ad5a0587b60f
djpillen@umich.edu <djpillen@umich.edu>
2017-06-28 17:00:30 -0400

    hide checkboxes

--- frontend/assets/accession_readonly_fields.js
+++ frontend/assets/accession_readonly_fields.js
@@ -1,17 +1,26 @@
 $(function () {
   var hidden_sections = ["accession_dates_", "accession_subjects_",
-  						"accession_rights_statements_", "accession_classifications_"]
+                        "accession_rights_statements_", "accession_classifications_"];
   
   hidden_sections.map(function (section) {
-  	$('section[id$=' + section + ']').hide();
-  	$('li[class^=sidebar-entry][class$=' + section + ']').hide();
+    $('section[id$=' + section + ']').hide();
+    $('li[class^=sidebar-entry][class$=' + section + ']').hide();
+  });
+
+  var hidden_fields = ["accession_publish_", "accession_restrictions_apply_", 
+                      "accession_access_restrictions_", "accession_use_restrictions_",
+                      "accession_collection_management__rights_determined_"];
+
+  hidden_fields.map(function (field) {
+    var fld_label = $("[for='" + field + "']");
+    fld_label.parent().hide();
   });
   
   var readonly_fields = ["accession_title_", "accession_inventory_", 
-            			"accession_retention_rule_", "accession_user_defined__string_1_", 
-            			"accession_restrictions_apply_", "accession_publish_",
-            			"accession_access_restrictions_", "accession_resource_type_",
-            			"accession_use_restrictions_"];
+                        "accession_retention_rule_", "accession_user_defined__string_1_", 
+                        "accession_restrictions_apply_", "accession_publish_",
+                        "accession_access_restrictions_", "accession_resource_type_",
+                        "accession_use_restrictions_"];
 
   readonly_fields.map(function (field) {
     $("#".concat(field)).attr("readonly", "readonly");
